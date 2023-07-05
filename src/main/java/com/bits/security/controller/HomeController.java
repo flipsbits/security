fa56ab4ba84c4bb139df66534183316aea1dcb17
Andre Guerlain <apguerlain@gmail.com>
2023-07-05 12:16:53 -0400

    initial commit

---
+++ src/main/java/com/bits/security/controller/HomeController.java
@@ -0,0 +1,20 @@
+/***************************************************************************
+ * COPYRIGHT (C) 2012-2023, Rapid7 LLC, Boston, MA, USA.
+ * All rights reserved. This material contains unpublished, copyrighted
+ * work including confidential and proprietary information of Rapid7.
+ **************************************************************************/
+package com.bits.security.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping
+    public String hello(Principal principal) {
+        return "Hello, " + principal.getName();
+    }
+}
